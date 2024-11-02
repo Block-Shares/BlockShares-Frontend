9ff9477b3d43a1f1050c0b3f883c4e0ee590bda8
Block-Shares <sharesblock@gmail.com>
2024-11-02 14:06:06 +0100

    Update landing-page.tsx

--- pages/landing-page.tsx
+++ pages/landing-page.tsx
@@ -282,7 +282,7 @@ export function LandingPage() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-emerald-800 z-10">
         <p className="text-xs text-gray-400">
-          © 2024 TokenizedBiz. All rights reserved.
+          © 2024 Blockshares. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link
